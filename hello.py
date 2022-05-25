c8bc9e5e49f216697df443106b2f856e69c6f79b
farom <farom57@gmail.com>
2022-05-25 14:03:25 +0200

    ASCOM Methods Common To All Devices

--- hello.py
+++ hello.py
@@ -1,41 +1,165 @@
 from flask import Flask, request
 
 app = Flask(__name__)
-ServerTransactionID = 0
+server_transaction_id = 0
+connected = False
 
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
 
+# ASCOM Methods Common To All Devices
 
-@app.route('/api/v1/dome/0/<name>', methods=['POST', 'GET', 'PUT'])
-def test(name):
-    # error = None
-    ServerTransactionID = +1
-    if request.method == 'GET':
-        print(
-            f"{name}, {request.method} {request.get_data()} {request.view_args} {request.args} {request.args.get('ClientID')}")
-        return 'req'
-    if request.method == 'POST':
-        searchword = request.form['key']
-        req = request.get_json()
-        print('name: %s, post: %s, %s', name, searchword, req)
+@app.route('/api/v1/dome/0/action', methods=['PUT'])
+def action():
+    global server_transaction_id
+    server_transaction_id += 1
+    req = {k.lower(): v for k, v in request.form.items()}
+    client_id = int(req.get('clientid'))
+    client_transaction_id = int(req.get('clienttransactionid'))
+    ret = {"ClientTransactionID": client_transaction_id, "ServerTransactionID": server_transaction_id,
+           "ErrorNumber": 0x400, "ErrorMessage": "Not implemented"}
+    return ret
 
-        return searchword
-    if request.method == 'PUT':
-        print(f"{name}, {request.method} {request.form} ")
-        # case insensitivise the parameters
-        req = {k.lower(): v for k, v in request.form.items()}
-        ClientID = int(req.get('clientid'))
-        ClientTransactionID = int(req.get('clienttransactionid'))
 
-        print(f"{req} ")
-        print(f"{ClientID} {ClientTransactionID} ")
-        ret = {"ClientTransactionID": ClientTransactionID, "ServerTransactionID": ServerTransactionID,
+@app.route('/api/v1/dome/0/commandblind', methods=['PUT'])
+def commandblind():
+    global server_transaction_id
+    server_transaction_id += 1
+    req = {k.lower(): v for k, v in request.form.items()}
+    client_id = int(req.get('clientid'))
+    client_transaction_id = int(req.get('clienttransactionid'))
+    ret = {"ClientTransactionID": client_transaction_id, "ServerTransactionID": server_transaction_id,
+           "ErrorNumber": 0x400, "ErrorMessage": "Not implemented"}
+    return ret
+
+
+@app.route('/api/v1/dome/0/commandbool', methods=['PUT'])
+def commandbool():
+    global server_transaction_id
+    server_transaction_id += 1
+    req = {k.lower(): v for k, v in request.form.items()}
+    client_id = int(req.get('clientid'))
+    client_transaction_id = int(req.get('clienttransactionid'))
+    ret = {"ClientTransactionID": client_transaction_id, "ServerTransactionID": server_transaction_id,
+           "ErrorNumber": 0x400, "ErrorMessage": "Not implemented"}
+    return ret
+
+
+@app.route('/api/v1/dome/0/commandstring', methods=['PUT'])
+def commandstring():
+    global server_transaction_id
+    server_transaction_id += 1
+    req = {k.lower(): v for k, v in request.form.items()}
+    client_id = int(req.get('clientid'))
+    client_transaction_id = int(req.get('clienttransactionid'))
+    ret = {"ClientTransactionID": client_transaction_id, "ServerTransactionID": server_transaction_id,
+           "ErrorNumber": 0x400, "ErrorMessage": "Not implemented"}
+    return ret
+
+
+@app.route('/api/v1/dome/0/connected', methods=['PUT'])
+def connected_put():
+    global server_transaction_id
+    global connected
+    server_transaction_id += 1
+    req = {k.lower(): v for k, v in request.form.items()}
+    client_id = int(req.get('clientid'))
+    client_transaction_id = int(req.get('clienttransactionid'))
+
+    if req.get('connected').lower() == 'true':
+        connected = True
+        ret = {"ClientTransactionID": client_transaction_id, "ServerTransactionID": server_transaction_id,
                "ErrorNumber": 0, "ErrorMessage": ""}
-        print(ret)
-        return ret
-    # the code below is executed if the request method
-    # was GET or the credentials were invalid
-    return 'plouf'
+    elif req.get('connected').lower() == 'false':
+        connected = False
+        ret = {"ClientTransactionID": client_transaction_id, "ServerTransactionID": server_transaction_id,
+               "ErrorNumber": 0, "ErrorMessage": ""}
+    else:
+        ret = {"ClientTransactionID": client_transaction_id, "ServerTransactionID": server_transaction_id,
+               "ErrorNumber": 0x401, "ErrorMessage": "Invalid value"}
+
+    return ret
+
+
+@app.route('/api/v1/dome/0/connected', methods=['GET'])
+def connected_get():
+    global server_transaction_id
+    global connected
+    server_transaction_id += 1
+    req = {k.lower(): v for k, v in request.args.items()}
+    client_id = int(req.get('clientid'))
+    client_transaction_id = int(req.get('clienttransactionid'))
+
+    ret = {"Value":connected, "ClientTransactionID": client_transaction_id, "ServerTransactionID": server_transaction_id,
+           "ErrorNumber": 0, "ErrorMessage": ""}
+    return ret
+
+@app.route('/api/v1/dome/0/description', methods=['GET'])
+def description():
+    global server_transaction_id
+    server_transaction_id += 1
+    req = {k.lower(): v for k, v in request.args.items()}
+    client_id = int(req.get('clientid'))
+    client_transaction_id = int(req.get('clienttransactionid'))
+    ret = {"Value":"Astrospace cupola", "ClientTransactionID": client_transaction_id, "ServerTransactionID": server_transaction_id,
+           "ErrorNumber": 0, "ErrorMessage": ""}
+    return ret
+
+@app.route('/api/v1/dome/0/driverinfo', methods=['GET'])
+def driverinfo():
+    global server_transaction_id
+    server_transaction_id += 1
+    req = {k.lower(): v for k, v in request.args.items()}
+    client_id = int(req.get('clientid'))
+    client_transaction_id = int(req.get('clienttransactionid'))
+    ret = {"Value":"Driver for written by Romain Fafet", "ClientTransactionID": client_transaction_id, "ServerTransactionID": server_transaction_id,
+           "ErrorNumber": 0, "ErrorMessage": ""}
+    return ret
+
+@app.route('/api/v1/dome/0/driverversion', methods=['GET'])
+def driverversion():
+    global server_transaction_id
+    server_transaction_id += 1
+    req = {k.lower(): v for k, v in request.args.items()}
+    client_id = int(req.get('clientid'))
+    client_transaction_id = int(req.get('clienttransactionid'))
+    ret = {"Value":"v0.0", "ClientTransactionID": client_transaction_id, "ServerTransactionID": server_transaction_id,
+           "ErrorNumber": 0, "ErrorMessage": ""}
+    return ret
+
+@app.route('/api/v1/dome/0/interfaceversion', methods=['GET'])
+def interfaceversion():
+    global server_transaction_id
+    server_transaction_id += 1
+    req = {k.lower(): v for k, v in request.args.items()}
+    client_id = int(req.get('clientid'))
+    client_transaction_id = int(req.get('clienttransactionid'))
+    ret = {"Value":0, "ClientTransactionID": client_transaction_id, "ServerTransactionID": server_transaction_id,
+           "ErrorNumber": 0, "ErrorMessage": ""}
+    return ret
+
+@app.route('/api/v1/dome/0/name', methods=['GET'])
+def name():
+    global server_transaction_id
+    server_transaction_id += 1
+    req = {k.lower(): v for k, v in request.args.items()}
+    client_id = int(req.get('clientid'))
+    client_transaction_id = int(req.get('clienttransactionid'))
+    ret = {"Value": "Cupola", "ClientTransactionID": client_transaction_id, "ServerTransactionID": server_transaction_id,
+           "ErrorNumber": 0, "ErrorMessage": ""}
+    return ret
+
+@app.route('/api/v1/dome/0/supportedactions', methods=['GET'])
+def supportedactions():
+    global server_transaction_id
+    server_transaction_id += 1
+    req = {k.lower(): v for k, v in request.args.items()}
+    client_id = int(req.get('clientid'))
+    client_transaction_id = int(req.get('clienttransactionid'))
+    ret = {"Value": [], "ClientTransactionID": client_transaction_id, "ServerTransactionID": server_transaction_id,
+           "ErrorNumber": 0, "ErrorMessage": ""}
+    return ret
+
+
