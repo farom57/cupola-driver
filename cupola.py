1a1041439e8d0c4ebc5a0a16731546e6a96f4182
farom <farom57@gmail.com>
2022-05-26 21:03:14 +0200

    Retour de connexion ok

--- cupola.py
+++ cupola.py
@@ -3,9 +3,11 @@ from bleak import BleakScanner, BleakClient
 import struct
 from threading import Thread
 
+
 class Cupola(object):
 
     def __init__(self):
+        self._flask_loop = None
         self._connected = False
         self._address = None
         self._client = None
@@ -13,23 +15,26 @@ class Cupola(object):
         self._HEAD_UUID = "c3fe2f77-e1c8-4b1c-a0f3-ef88d050313a"
         self._ALIVE_UUID = "c3fe2f77-e1c8-4b1c-a0f3-ef88d0503151"
 
-        #self._loop = asyncio.get_event_loop()
+        # self._loop = asyncio.get_event_loop()
         self._loop = asyncio.new_event_loop()
         asyncio.set_event_loop(self._loop)
+
         def bleak_thread(loop):
             asyncio.set_event_loop(loop)
             loop.run_forever()
+
         self._thread = Thread(target=bleak_thread, args=(self._loop,))
         self._thread.start()
 
         self._disconnected_event = asyncio.Event()
+        self._connected_event = asyncio.Event()
 
     def __del__(self):
         print('Stopping')
         self._loop.call_soon_threadsafe(self._loop.stop)
 
     async def connect(self):
-        #try:
+        # try:
         if self._client is not None:
             if self._client.is_connected:
                 print('already connected')
@@ -37,7 +42,6 @@ class Cupola(object):
 
         print('[scanning]')
         devices = await BleakScanner.discover()
-
         for d in devices:
             print(d)
             if d.name == "Cupola":
@@ -49,40 +53,26 @@ class Cupola(object):
             print("Error: Cupola not found")
             return False
         print('[connecting]')
-
-        print('[running]')
         asyncio.run_coroutine_threadsafe(self.maintain_connection(), self._loop)
-        print('[exiting]')
 
-        # while self._client.is_connected:
-        #     try:
-        #         val = await self._client.read_gatt_char(self._HEAD_UUID)
-        #         # print(val)
-        #         valf = struct.unpack('f', val)[0]
-        #         print(valf)
-        #
-        #     except Exception as e:
-        #         print(e)
-        #     await asyncio.sleep(1)
-
-        return True
-
-        #except Exception as e:
-        #    print(e)
-        #    return False
+        self._flask_loop = asyncio.get_event_loop()
+        print('[waiting connection]')
+        await self._connected_event.wait()
+        print('[returned]')
+        return self._connected
 
     async def maintain_connection(self):
-        res = await self._client.connect()
-        print(res)
+        self._connected = await self._client.connect()
+
+        self._flask_loop.call_soon_threadsafe(self._connected_event.set)
+        if not self._connected:
+            return
+
         print('[connected]')
         await self._client.write_gatt_char(self._STATE_UUID, bytearray([5]))
         print('[running]')
         await self._disconnected_event.wait()
-        # while True:
-        #     print("Connected:", self._client.is_connected)
-        #     await asyncio.sleep(1)
-        #     await self._client.write_gatt_char(self._ALIVE_UUID, bytearray([5]))
-        print('[stop]')
+        print('[disconnected]')
 
     def disconnected_callback(self, client):
         print("Disconnected callback called!")
@@ -96,5 +86,3 @@ class Cupola(object):
             await self._client.disconnect()
             print('[disconnected]')
             return True
-
-
